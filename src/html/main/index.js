70bb42658fd67ed6147c70955c2edc85be9706cf
sbpark88 <saebyulpark88@gmail.com>
2024-03-09 20:00:55 +0900

    Close #4: add navigation with sticky and blur

--- src/html/main/index.js
+++ src/html/main/index.js
@@ -1,4 +1,5 @@
 import Common from "../Common";
 import "../../js/components/header/index";
+import "../../js/components/nav/index";
 
 new Common();
---
+++ src/js/components/nav/index.js
@@ -0,0 +1,8 @@
+// @ts-check
+
+import "../../../css/nav/index.scss";
+
+import { init } from "../../utils/render";
+import template from "./index.html";
+
+init(template, "nav")(() => {});
