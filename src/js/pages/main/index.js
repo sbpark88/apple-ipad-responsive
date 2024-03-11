ba52c5c2a5aa1f0d109ff4601d2c28d5e3cc4d7b
sbpark88 <saebyulpark88@gmail.com>
2024-03-11 21:51:46 +0900

    Close #8: sprite image with keyframes

--- src/js/pages/main/index.js
+++ src/js/pages/main/index.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 import "../../../css/pages/main/index.scss";
+import "../../../css/components/features.scss";
 
 import { init } from "../../utils/render";
 import template from "./index.html";
