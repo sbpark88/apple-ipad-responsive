156941565300c41035df520f72538b68e4682c18
sbpark88 <saebyulpark88@gmail.com>
2024-03-16 22:42:47 +0900

    Chore: fix typo

--- src/js/pages/main/AccessoryShop.js
+++ src/js/pages/main/AccessoryShop.js
@@ -6,7 +6,7 @@ import ipadAccessories from "../../../assets/images/ipad_accessories.jpg";
 import { BlockLink } from "../../components/Common";
 
 const children = `
-<h1>iPad의 능력일 키워주는<br />액세서리</h1>
+<h1>iPad의 능력을 키워주는<br />액세서리</h1>
 <p>케이스, 커버, Apple Pencil, AirPods 등 다양한 액세서리를 만나보세요.</p>
 ${BlockLink({ text: "iPad 액세서리 쇼핑하기" })}
 ${Image({ imgSrc: ipadAccessories, imgAlt: "다양한 액세서리 이미지" })}
