4c55f24be4cb57154a39b2e6878607224358fdc0
sbpark88 <saebyulpark88@gmail.com>
2024-03-19 22:53:41 +0900

    Close #22: responsive-nav

--- src/js/components/nav/index.js
+++ src/js/components/nav/index.js
@@ -2,7 +2,33 @@
 
 import "../../../css/components/nav/index.scss";
 
-import { init } from "../../utils/render";
+import { $, init } from "../../utils/render";
 import template from "./index.html";
+import { preventScroll, toggleClassName } from "../../utils/styleHelper";
+import { eventBind } from "../../utils/eventBinding";
 
 init(template, "nav")(() => {});
+
+const header = $("header");
+
+const navEl = $("nav");
+const navShadowEl = $("nav .shadow");
+const navMenuToggleEl = $("nav .menu-toggle");
+
+const [showMenu, hideMenu] = toggleClassName({
+  $el: navEl,
+  className: "menuing",
+});
+
+const clickNavMenuToggleEl = (event) => {
+  event.stopPropagation();
+  if (navEl.classList.contains("menuing")) {
+    hideMenu();
+  } else {
+    showMenu();
+  }
+};
+
+eventBind({ $el: navMenuToggleEl, listener: clickNavMenuToggleEl });
+eventBind({ $el: navShadowEl, listener: hideMenu });
+eventBind({ $el: header, listener: hideMenu });
